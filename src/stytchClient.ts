6cc14b06bf881e58defc86f2c2d47b3d0d72affc
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-06-02 13:34:05 +0530

    update stytch keys

--- src/stytchClient.ts
+++ src/stytchClient.ts
@@ -2,8 +2,8 @@
 import { Client } from 'stytch';
 
 const clientConfig = {
-  project_id: "project-test-71af05db-7a36-43d9-9b7d-49b230681184",
-  secret:"secret-test-Im4KnH0vHUwDiwFv_xWpcloKWpyw3yijowc="
+  project_id: "project-test-6a34d2d7-e20a-43ac-a808-c48e45ac23cc",
+  secret:"secret-test-akdK0dodjbvwP6BhWuyEZNYx9V0Gu7LJ3Xc="
 };
 
 const client = new Client(clientConfig);
